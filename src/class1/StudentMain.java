d67fd8424ffcf7c143ad2034d06a64de4324f174
hj <parkhj062@gmail.com>
2025-12-24 19:54:42 +0900

    [9. 배열 도입 - 시작]

--- src/class1/StudentMain.java
+++ src/class1/StudentMain.java
@@ -4,8 +4,10 @@ public class StudentMain {
     public static void main(String[] args) {
         Student student1 = new Student("student1", 15, 90); // student instance reference
         Student student2 = new Student("student2", 16, 80);
+        Student[] students = {student1, student2}; // instance reference array
 
-        System.out.println(student1);
-        System.out.println(student2);
+        for (Student student : students) {
+            System.out.println(student);
+        }
     }
 }
