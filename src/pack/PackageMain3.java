12d38e0d1a93b108dad95296e212d17f6ffc127a
hj <parkhj062@gmail.com>
2025-12-24 23:01:19 +0900

    [37. 패키지 - import]
    - pack.* : import all class
    - pack.b.User: duplicated class name

--- src/pack/PackageMain3.java
+++ src/pack/PackageMain3.java
@@ -2,9 +2,10 @@ package pack;
 
 import pack.a.User;
 
-public class PackageMain1 {
+public class PackageMain3 {
     public static void main(String[] args) {
         Data data = new Data();
         User user = new User();
+        pack.b.User user1 = new pack.b.User();
     }
 }
