fec1bf57fab9596297f81d3d4e95d624093f2fe4
hj <parkhj062@gmail.com>
2025-12-24 23:30:01 +0900

    [41. 접근 제어자 이해1]

---
+++ src/access/Speaker.java
@@ -0,0 +1,28 @@
+package access;
+
+public class Speaker {
+    int volume;
+
+    public Speaker(int volume) {
+        this.volume = volume;
+    }
+
+    void volumeUp() {
+        if(volume >= 100) {
+            System.out.println("Volume is full");
+            return;
+        }
+
+        volume+= 10;
+        System.out.println("Speaker.volumeUp");
+    }
+
+    void volumeDown() {
+        volume -= 10;
+        System.out.println("Speaker.volumeDown");
+    }
+
+    void showVolume() {
+        System.out.println("Speaker.volume: " + volume);
+    }
+}
---
+++ src/access/SpeakerMain.java
@@ -0,0 +1,19 @@
+package access;
+
+public class SpeakerMain {
+    public static void main(String[] args) {
+        Speaker speaker = new Speaker(90);
+
+        speaker.volumeUp();
+        speaker.showVolume();
+
+        speaker.volumeUp();
+        speaker.showVolume();
+
+        speaker.volumeDown();
+        speaker.showVolume();
+
+        speaker.volume = 200; // direct access
+        speaker.showVolume();
+    }
+}
