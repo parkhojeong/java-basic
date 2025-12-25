1176b234dced1fd96075e567566d31fe0294fdfc
hj <parkhj062@gmail.com>
2025-12-25 23:16:45 +0900

    [93. 다형성 - 역할과 구현 예제2]
    - add new car

--- src/poly/car0/Driver.java
+++ src/poly/car0/Driver.java
@@ -2,15 +2,28 @@ package poly.car0;
 
 public class Driver {
     private K3Car k3Car;
+    private Model3Car model3Car;
 
     public void setK3Car(K3Car k3Car) {
         this.k3Car = k3Car;
     }
 
+    public void setModel3Car(Model3Car model3Car) {
+        this.model3Car = model3Car;
+    }
+
     public void drive() {
-        System.out.println("Driver.drive");
-        k3Car.startEngine();
-        k3Car.pressAccelerator();
-        k3Car.stopEngine();
+        if(k3Car != null) {
+            System.out.println("Driver.drive");
+            k3Car.startEngine();
+            k3Car.pressAccelerator();
+            k3Car.stopEngine();
+        } else if(model3Car != null) {
+            System.out.println("Driver.drive");
+            model3Car.startEngine();
+            model3Car.pressAccelerator();
+            model3Car.stopEngine();
+        }
+
     }
 }
