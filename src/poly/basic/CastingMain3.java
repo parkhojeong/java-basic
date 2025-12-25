1bdb5917bcc97ba2e55ea886755b5444dd201e8b
hj <parkhj062@gmail.com>
2025-12-25 20:09:31 +0900

    [77. 캐스팅의 종류]
    - up casting
     - Parent parent = child;

---
+++ src/poly/basic/CastingMain3.java
@@ -0,0 +1,12 @@
+package poly.basic;
+
+public class CastingMain3 {
+    public static void main(String[] args) {
+        Child child = new Child();
+        Parent parent1 = (Parent) child;
+        Parent parent2 = child; // recommended
+
+        parent1.parentMethod();
+        parent2.parentMethod();
+    }
+}
