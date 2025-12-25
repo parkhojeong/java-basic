116f445b606dab33f563cf1f3337d913286c9149
hj <parkhj062@gmail.com>
2025-12-25 15:38:13 +0900

    [58. static 메서드3]
    - static import

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,15 +1,17 @@
 package static2;
 
+import static static2.DecoData.staticCall;
+
 public class DecoDataMain {
     public static void main(String[] args) {
         System.out.println("1. staticCall()");
-        DecoData.staticCall();
+        staticCall();
 
         System.out.println("2. instanceCall()");
         DecoData data = new DecoData();
         data.instanceCall();
 
         System.out.println("3. staticCall(data)");
-        DecoData.staticCall(data);
+        staticCall(data);
     }
 }
