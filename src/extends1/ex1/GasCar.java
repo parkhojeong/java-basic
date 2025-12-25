963a4cad2e9a41e64faacdb86f6a1f5fa44148a3
hj <parkhj062@gmail.com>
2025-12-25 16:53:31 +0900

    [65. 상속 - 시작]

---
+++ src/extends1/ex1/GasCar.java
@@ -0,0 +1,11 @@
+package extends1.ex1;
+
+public class GasCar {
+    public void move() {
+        System.out.println("Gas car moving");
+    }
+
+    public void fillGas() {
+        System.out.println("Gas car filling gas");
+    }
+}
