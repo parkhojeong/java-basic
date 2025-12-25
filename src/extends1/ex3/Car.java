3a659ef29b99e6bbc721a9c8257f4bcbae7771ee
hj <parkhj062@gmail.com>
2025-12-25 17:19:11 +0900

    [68. 상속과 기능 추가]

---
+++ src/extends1/ex3/Car.java
@@ -0,0 +1,12 @@
+package extends1.ex3;
+
+public class Car {
+    public void move() {
+        System.out.println("Car moving");
+    }
+
+    public void openDoor() {
+        System.out.println("Car opening door");
+    }
+
+}
